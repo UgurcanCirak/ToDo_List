84118541d7f0057f95785a77db03addf740c2de9
UgurcanCirak <102437415+UgurcanCirak@users.noreply.github.com>
2023-09-30 16:27:52 +0300

    Initial Commit

---
+++ ToDo_List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo_List
+//
+//  Created by Uğurcan Çırak on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
